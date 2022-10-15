3d8dae6bfe7518196c459281ec86a379412e1580
johnsk8 <Ludin@DESKTOP-BAB0NU4.(none)>
2022-10-15 13:39:40 -0700

    Added initial fetch request

--- simple-status-ui/src/App.js
+++ simple-status-ui/src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      results: [],
+      dataIsLoaded: false
+    };
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:8000/healthz', {
+        method: 'GET',
+        // mode: 'no-cors'
+        header: {
+          'Access-Control-Allow-Origin': 'http://localhost:8000'
+        }
+      }
+    ) // TODO: make the url into an env var
+    .then((res) => res.json())
+    .then((json) => {
+      console.log(json);
+      this.setState({
+        results: json,
+        dataIsLoaded: true
+      })
+    })
+  }
+
+  render() {
+    const { results, dataIsLoaded } = this.state;
+
+    if (!dataIsLoaded) {
+      return (
+        <div>
+          <h1>Fetching data. Please wait...</h1>
+        </div>
+      )
+    } else {
+      console.log(results)
+      return (
+        <div>
+          <h1>Fetched data from an api in react</h1>
+        </div>
+      )
+    }
+  }
 }
+
+// function App() {
+//   return (
+//     <div className="App">
+//     </div>
+//   );
+// }
 
 export default App;
