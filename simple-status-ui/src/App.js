ead9e0e597cecebd2cce49d38dfed20025817a29
johnsk8 <Ludin@DESKTOP-BAB0NU4.(none)>
2022-10-16 17:31:45 -0700

    Added test accordion for services array

--- simple-status-ui/src/App.js
+++ simple-status-ui/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
+import Accordion from 'react-bootstrap/Accordion';
 import './App.css';
 
 class App extends React.Component {
@@ -57,26 +58,52 @@ class App extends React.Component {
         <div className = "App">
           <link
             rel="stylesheet"
-            href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-            integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
-            crossOrigin="anonymous"
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css"
+            integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi"
+            crossorigin="anonymous"
           />
           <Container>
-            <Row className="justify-content-md-center" >
+            <Row className="justify-content-md-center">
               <h1 style={{marginTop: 50, marginBottom: 50, width: '25rem'}}>Simple Status Page</h1>
             </Row>
-            <Row>
-              {
+            <Row className="justify-content-md-center">
+              <Accordion alwaysOpen>
+                <Accordion.Item eventKey="0">
+                  <Accordion.Header>Accordion Item #1</Accordion.Header>
+                  <Accordion.Body>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                    minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                    aliquip ex ea commodo consequat. Duis aute irure dolor in
+                    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                    pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                    culpa qui officia deserunt mollit anim id est laborum.
+                  </Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey="1">
+                  <Accordion.Header>Accordion Item #2</Accordion.Header>
+                  <Accordion.Body>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                    minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                    aliquip ex ea commodo consequat. Duis aute irure dolor in
+                    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                    pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                    culpa qui officia deserunt mollit anim id est laborum.
+                  </Accordion.Body>
+                </Accordion.Item>
+              </Accordion>
+              {/* {
                 results['Services'].map((item) => (
-                  <ol key = 'Services'>
-                    <li>Service Name: {item.name}</li>
-                    <li>Service Response Status Code: {item.status}</li>
-                    <li>Service URL: {item.url}</li>
-                    <li>Service Category: {item.category}</li>
-                    <li>Service Description: {item.description}</li>
-                  </ol>
+                  // <ol key = 'Services'>
+                  //   <li>Service Name: {item.name}</li>
+                  //   <li>Service Response Status Code: {item.status}</li>
+                  //   <li>Service URL: {item.url}</li>
+                  //   <li>Service Category: {item.category}</li>
+                  //   <li>Service Description: {item.description}</li>
+                  // </ol>
                 ))
-              }
+              } */}
             </Row>
           </Container>
         </div>
