6f325fa0f0d000ec85d2c2c4f15e5c7d98e073e4
johnsk8 <Ludin@DESKTOP-BAB0NU4.(none)>
2022-10-20 14:48:04 -0700

    Added system status component

--- simple-status-ui/src/pages/home/index.js
+++ simple-status-ui/src/pages/home/index.js
@@ -3,27 +3,8 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Accordion from 'react-bootstrap/Accordion';
 import Badge from 'react-bootstrap/Badge';
-import Alert from 'react-bootstrap/Alert';
+import SystemStatus from '../../components/system-status';
 import axios from 'axios';
-
-function SystemStatus(results) {
-  var systemsOperational = true;
-  for(let r of results['results']['Services']) {
-    if(r.status !== 200) {
-      systemsOperational = false;
-      break;
-    }
-  }
-
-  return systemsOperational === true ?
-    <Alert variant="success">
-      <Alert.Heading>All Systems Operational</Alert.Heading>
-    </Alert>
-    :
-    <Alert variant="danger">
-      <Alert.Heading>System Failure</Alert.Heading>
-    </Alert>;
-}
 
 class Home extends Component {
   constructor(props) {
