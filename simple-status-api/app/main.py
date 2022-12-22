e8e93fe49f985d6d5a5286e89bed41741450b276
johnsk8 <Ludin@DESKTOP-BAB0NU4.(none)>
2022-12-22 15:17:53 -0800

    Added init get crd from k8s api

--- simple-status-api/app/main.py
+++ simple-status-api/app/main.py
@@ -4,6 +4,9 @@ from typing import Optional
 from fastapi.middleware.cors import CORSMiddleware
 from .config import settings, default_service
 import requests
+import kubernetes.client
+from kubernetes.client.rest import ApiException
+from pprint import pprint
 
 app = FastAPI()
 
@@ -30,20 +33,14 @@ class Service(BaseModel):
 class ServiceStatus(Service):
     status: int
 
+
 # TODO: Place this in a location like app/internal/get_services.py
 def get_services():
     try:
-        # TODO: Call the K8s API to get requested services
-        return [
-                {
-                    "name": "K8s API",
-                    "url": "http://localhost:8000/healthz",
-                    "category": "API",
-                    "description": "A simple status page."
-                }
-            ]
+        # Call the K8s API to get requested services
+        return get_namespaced_custom_object()
     except:
-        # Otherwise use the default service
+        # Otherwise use the default service which is self
         return [
                 {
                     "name": default_service.name,
@@ -52,6 +49,34 @@ def get_services():
                     "description": default_service.description
                 }
             ]
+
+
+# TODO: Place this in a location like app/internal/get_namespaced_custom_object.py
+def get_namespaced_custom_object():
+    configuration = kubernetes.client.Configuration()
+    # Configure API key authorization: BearerToken
+    configuration.api_key['authorization'] = 'YOUR_API_KEY'
+    # Uncomment below to setup prefix (e.g. Bearer) for API key, if needed
+    # configuration.api_key_prefix['authorization'] = 'Bearer'
+
+    # Defining host is optional and default to http://localhost
+    configuration.host = "http://localhost"
+
+    # Enter a context with an instance of the API kubernetes.client
+    with kubernetes.client.ApiClient(configuration) as api_client:
+        # Create an instance of the API class
+        api_instance = kubernetes.client.CustomObjectsApi(api_client)
+        group = 'group_example' # str | the custom resource's group
+        version = 'version_example' # str | the custom resource's version
+        namespace = 'namespace_example' # str | The custom resource's namespace
+        plural = 'plural_example' # str | the custom resource's plural name. For TPRs this would be lowercase plural kind.
+        name = 'name_example' # str | the custom object's name
+
+    try:
+        api_response = api_instance.get_namespaced_custom_object(group, version, namespace, plural, name)
+        pprint(api_response)
+    except ApiException as e:
+        print("Exception when calling CustomObjectsApi->get_namespaced_custom_object: %s\n" % e)
 
 
 # TODO: Place this in a location like app/routers/status.py
@@ -72,7 +97,7 @@ def status():
                 description=s["description"],
                 status=r.status_code
             )
-            print("INFO      Service info:", service_status)
+            print("INFO      Service response:", service_status)
             services_statuses.append(service_status)
         return {"Services": services_statuses}
 
@@ -80,6 +105,7 @@ def status():
         raise HTTPException(status_code=404, message="A service or services not found")
 
 
+# TODO: Place this in a location like app/routers/healthz.py
 # Health check
 @app.get("/healthz")
 def healthz():
