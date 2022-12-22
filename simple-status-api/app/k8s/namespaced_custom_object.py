5063a0dc4d6eea8360f6b87869fe2389f54e8b4d
johnsk8 <Ludin@DESKTOP-BAB0NU4.(none)>
2022-12-22 14:50:45 -0800

    Updated status ep to be get and use default service

--- simple-status-api/app/k8s/namespaced_custom_object.py
+++ simple-status-api/app/k8s/namespaced_custom_object.py
@@ -1,17 +1,29 @@
+import kubernetes.client
+from kubernetes.client.rest import ApiException
 from pprint import pprint
-from kubernetes import client, config
 
 def get_namespaced_custom_object():
-    config.load_kube_config()
+    configuration = kubernetes.client.Configuration()
+    # Configure API key authorization: BearerToken
+    configuration.api_key['authorization'] = 'YOUR_API_KEY'
+    # Uncomment below to setup prefix (e.g. Bearer) for API key, if needed
+    # configuration.api_key_prefix['authorization'] = 'Bearer'
 
-    api = client.CustomObjectsApi()
+    # Defining host is optional and default to http://localhost
+    configuration.host = "http://localhost"
 
-    resource = api.get_namespaced_custom_object(
-        group="stable.example.com",
-        version="v1",
-        name="my-new-cron-object",
-        namespace="default",
-        plural="crontabs",
-    )
-    print("Resource details:")
-    pprint(resource)
+    # Enter a context with an instance of the API kubernetes.client
+    with kubernetes.client.ApiClient(configuration) as api_client:
+        # Create an instance of the API class
+        api_instance = kubernetes.client.CustomObjectsApi(api_client)
+        group = 'group_example' # str | the custom resource's group
+        version = 'version_example' # str | the custom resource's version
+        namespace = 'namespace_example' # str | The custom resource's namespace
+        plural = 'plural_example' # str | the custom resource's plural name. For TPRs this would be lowercase plural kind.
+        name = 'name_example' # str | the custom object's name
+
+    try:
+        api_response = api_instance.get_namespaced_custom_object(group, version, namespace, plural, name)
+        pprint(api_response)
+    except ApiException as e:
+        print("Exception when calling CustomObjectsApi->get_namespaced_custom_object: %s\n" % e)
--- simple-status-api/app/main.py
+++ simple-status-api/app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from typing import Optional, List
 from fastapi.middleware.cors import CORSMiddleware
@@ -17,16 +17,6 @@ app.add_middleware(
     allow_headers=["*"]
 )
 
-# Default request body
-default_services = [
-    { 
-        "name": default_service.name,
-        "url": default_service.url,
-        "category": default_service.category,
-        "description": default_service.description
-    }
-]
-
 
 # The base class for a service
 class Service(BaseModel):
@@ -41,27 +31,52 @@ class ServiceStatus(Service):
     status: int
 
 
-# Get the status for services
-@app.post("/status")
-def status(services: List[Service] = default_services):
+def get_services():
     try:
+        # TODO: Call the K8s API to get requested services
+        return [
+                {
+                    "name": "K8s API",
+                    "url": "http://localhost:8000/healthz",
+                    "category": "API",
+                    "description": "A simple status page."
+                }
+            ]
+    except:
+        # Otherwise use the default service
+        return [
+                {
+                    "name": default_service.name,
+                    "url": default_service.url,
+                    "category": default_service.category,
+                    "description": default_service.description
+                }
+            ]
+
+
+# Get the status for services
+@app.get("/status")
+def status():
+    try:
+        services = get_services()
+
         services_statuses = []
         for s in services:
             print("INFO      Requested service:", s)
-            r = requests.get(s.url)
+            r = requests.get(s["url"])
             service_status = ServiceStatus(
-                name=s.name,
-                url=s.url,
-                category=s.category,
-                description=s.description,
+                name=s["name"],
+                url=s["url"],
+                category=s["category"],
+                description=s["description"],
                 status=r.status_code
             )
             print("INFO      Service info:", service_status)
             services_statuses.append(service_status)
         return {"Services": services_statuses}
 
-    except Exception as e:
-        print(e)
+    except:
+        raise HTTPException(status_code=404, detail="Not found")
 
 
 # Health check
